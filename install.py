e72e71c27b5f069b0fc36b94ec3598d06be6ab52
shadow522023 <134283520+shadow522023@users.noreply.github.com>
2023-05-28 19:39:17 +0700

    Add files via upload

---
+++ install.py
@@ -0,0 +1,14 @@
+import os
+import time
+
+print("install module______________________")
+os.system("pip install time")
+time.sleep(3)
+os.system("pip install os")
+time.sleep(3)
+os.system("pip install subprocess")
+time.sleep(3)
+os.system("pip install math")
+time.sleep(3)
+os.system("pip install corolama")
+print("______________________________________")
