8ec6501de7f5011624dc4cbe9ecdf93e4b6c69ed
shadow522023 <134283520+shadow522023@users.noreply.github.com>
2023-05-28 19:45:14 +0700

    Update install.py

--- install.py
+++ install.py
@@ -2,13 +2,5 @@ import os
 import time
 
 print("install module______________________")
-os.system("pip install time")
-time.sleep(3)
-os.system("pip install os")
-time.sleep(3)
-os.system("pip install subprocess")
-time.sleep(3)
-os.system("pip install math")
-time.sleep(3)
-os.system("pip install corolama")
+os.system("pip install colorama")
 print("______________________________________")
